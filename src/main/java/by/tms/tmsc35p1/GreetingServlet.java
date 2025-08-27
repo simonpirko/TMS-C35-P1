2d37b6db4fc80cd1735b5d23c1008e15b2a10a1f
Simon Pirko <sipi7gm@gmail.com>
2025-08-27 19:19:15 +0300

    LGTM

--- src/main/java/by/tms/tmsc35p1/GreetingServlet.java
+++ src/main/java/by/tms/tmsc35p1/GreetingServlet.java
@@ -13,6 +13,6 @@ public class GreetingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
-        resp.getWriter().println("Hello, " + name + "!");
+        resp.getWriter().printf("Hello, %s!", name);
     }
 }
