437883fb6d09312fddb9042b653ca8933b219a16
Yulia Kutsakova <yylia95@mail.ru>
2025-09-10 18:52:25 +0300

    gender

--- src/main/resources/db.migrations/V2__create_table_account_details.sql
+++ src/main/resources/db.migrations/V2__create_table_account_details.sql
@@ -6,8 +6,11 @@ CREATE TABLE IF NOT EXISTS account_details (
      website VARCHAR(255),
      birth_date DATE,
      avatar_url VARCHAR(500),
-     header_url VARCHAR(500)
+     header_url VARCHAR(500),
+     gender bytea
+
 );
+
 
 alter table account_details
     owner to postgres;
